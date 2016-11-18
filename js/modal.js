6ccd2f8b0b3cdae3cf93ae9522b2cf789ef46b39
Jon Kuperman <jon.kuperman@gmail.com>
2016-11-18 10:35:00 -0600

    answers

--- js/modal.js
+++ js/modal.js
@@ -10,9 +10,44 @@ cancelButton.addEventListener('click', close);
 // https://github.com/jkup/focusable
 
 function open() {
+  var previouslyFocused = document.activeElement;
+
+  modal.addEventListener('keydown', trapTabKey);
+
+  modalOverlay.addEventListener('click', close);
+
+  var focusableElements = modal.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]');
+
+  focusableElements = Array.prototype.slice.call(focusableElements);
+
+  var firstItem = focusableElements[0];
+  var lastItem = focusableElements[focusableElements.length -1];
+
   // Show the modal and overlay
   modal.style.display = 'block';
   modalOverlay.style.display = 'block';
+
+  firstItem.focus();
+
+  function trapTabKey(e) {
+    if (e.keyCode === 9) {
+      // Shift Tab
+      if (e.shiftKey) {
+        if (document.activeElement === firstItem) {
+          e.preventDefault();
+          lastItem.focus();
+        }
+      // Tab
+      } else {
+        if (document.activeElement === lastItem) {
+          e.preventDefault();
+          firstItem.focus();
+        }
+      }
+    } else if (e.keyCode === 27) {
+      close();
+    }
+  }
 }
 
 function close() {
